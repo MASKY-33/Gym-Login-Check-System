1caaf68b1af2e2031613ae4233fe7c944aca64d6
MASKY-33 <masky.somebody33@gmail.com>
2026-07-05 19:55:46 +0200

    Add files via upload

---
+++ gym-system/gym_system/gym_management/check_in.py
@@ -0,0 +1,23 @@
+active_members = {
+    101 : "Lauren",
+    103 : "Sarah",
+    105 : "Zack"
+}
+
+def check_in_member(member_id):
+
+    try:
+        id_number = int(member_id)
+
+        if id_number in active_members:
+            active_members[id_number]
+
+            with open("visitors_log.txt", "a") as file:
+                file.write(f"ID: {id_number} | name: {active_members[id_number]} has entered. \n")
+                return "Access granted! Welcome."
+        else:
+            return "Access denied: Unknown ID."
+    except ValueError:
+        return "Error: Only numbers are acceptable!"
+    except IOError:
+        return "System error: visitor could not be saved!"
---
+++ gym-system/gym_system/interaction.py
@@ -0,0 +1,11 @@
+from gym_management.check_in import check_in_member
+
+while True:
+    membership_request = input("Give your membership-ID here, (or type 'stop' to stop): ")
+
+    if membership_request == "stop":
+        print("Thankyou, Bye!")
+        break
+
+    membership_result = check_in_member(membership_request)
+    print(membership_result)
